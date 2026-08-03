083b4c303dfdca99490aa03d300666cc4a657b75
michaelhmiv <34044957+michaelhmiv@users.noreply.github.com>
2026-08-03 14:19:11 -0400

    feat: expose Railway nutrition compatibility repository

---
+++ src/nutrition-platform/index.ts
@@ -0,0 +1,58 @@
+export {
+    countMeals,
+    deleteMeal,
+    existingIdempotencyKeys,
+    getAllMeals,
+    getMealsByDate,
+    getMealsInRange,
+    insertMeal,
+    mealIdempotencyKey,
+    searchMeals,
+    updateMeal,
+} from "./meals.js";
+export {
+    alcoholTrackingEnabledFromProfile,
+    getAlcoholTrackingEnabled,
+    getPreferredDrinkUnit,
+    getPreferredWeightUnit,
+    getProfile,
+    getUserTimezone,
+    getWidgetsEnabled,
+    preferredDrinkUnitFromProfile,
+    upsertProfile,
+    widgetsEnabledFromProfile,
+} from "./profiles.js";
+export {
+    getNutritionGoals,
+    upsertNutritionGoals,
+} from "./goals.js";
+export {
+    deleteWater,
+    getWaterByDate,
+    getWaterInRange,
+    insertWater,
+} from "./water.js";
+export {
+    deleteWeight,
+    getLatestWeight,
+    getWeightByDate,
+    getWeightInRange,
+    insertWeight,
+    updateWeight,
+} from "./weight.js";
+export { deleteAllUserData } from "./account.js";
+export type {
+    Meal,
+    MealInput,
+    MealInsertResult,
+    NutritionGoals,
+    NutritionGoalsInput,
+    Profile,
+    ProfilePatch,
+    WaterEntry,
+    WaterInput,
+    WaterInsertResult,
+    WeightEntry,
+    WeightInput,
+    WeightInsertResult,
+} from "./types.js";
