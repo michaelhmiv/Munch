847f9a093a2636eb3550b21ea25bdbd42b82c0ba
michaelhmiv <34044957+michaelhmiv@users.noreply.github.com>
2026-08-27 07:46:03 -0400

    Register native Play Billing plugin

--- mobile/android/MainActivity.java
+++ mobile/android/MainActivity.java
@@ -8,6 +8,7 @@ public class MainActivity extends BridgeActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         registerPlugin(MunchSecureSessionPlugin.class);
+        registerPlugin(MunchPlayBillingPlugin.class);
         super.onCreate(savedInstanceState);
     }
 }
